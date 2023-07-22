fcf0ca16f6999dbce014a0c41109881d9a262836
Ubuntu <ubuntu@ip-172-31-95-242.ec2.internal>
2023-07-22 13:05:32 +0000

    first commit

---
+++ sshfoldertest/gr.tf
@@ -0,0 +1,23 @@
+njksndlvsmlkjsj\
+amsmknmlc zmx 
+
+njlkasnflkasdcklnsd,
+'mcmlsd ncmlsoiej4
+jlkc z,ceijopaw4
+lknalk n cmlm zmkpkop[de54'
+joknlkmopipoaw4
+l;jkonkjionas,c.;c4jjn
+nkl ml alkjdajkmc,smvkJOIQA
+JKNCLAMSMC, WQJWEJPOWQKL;QMM,;m,ms;lknalkSAMMC
+FOM
+FROM
+MMOINNKA
+MAINTAINER
+RUN]
+CMD
+\ECHO\]jkonkjionasNLJK
+KADKLAC,L ,L;P9UJ
+KLNM, .,. LKJOIPUIJ
+LKANFMLSCM,,LKJAD\
+NAMLSSNCM LKJWEFEOKJ
+]NMC SM,NQLKWJFDWKELM]
